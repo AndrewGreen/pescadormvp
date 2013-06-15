a22dc8836cfa2253fc830f33be0feb200ce74893
Andrew Green <andrew.green.df@gmail.com>
2013-06-15 10:49:40 -0500

    Beginning of core package documentation

---
+++ pescadormvp/src/main/java/mx/org/pescadormvp/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * <h2>Pescador MVP</h2>
+ * <p>
+ * Pescador MVP is a modular MVP framework for GWT that builds on standard
+ * MVP-related classes provided by GWT. Its main features include the
+ * association of places, activities, views and components using generics, and
+ * GIN-based dependency injection.
+ * </p>
+ */
+package mx.org.pescadormvp;
